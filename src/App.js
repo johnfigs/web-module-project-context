720e1e4cb76a2c6a4b89f066c315c99cdce13f9a
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-08-12 14:14:57 -0700

    Completed project setup and added addItem functionality

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+	console.log("cart", cart)
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
